ef178a55e896c9f7f3007d5e0555586d3ef6103a
a129924 <a129924@gmail.com>
2024-08-07 22:37:51 +0800

    feat: 將部分操作移至pipeline

--- news_crawler/spiders/cnyes_spider.py
+++ news_crawler/spiders/cnyes_spider.py
@@ -18,34 +18,46 @@ class CnyesSpider(Spider):
         "https://api.cnyes.com/media/api/v1/newslist/category/headline?limit=30&page=1"
     ]
     custom_settings: dict[str, Any] = {"DEFAULT_REQUEST_HEADERS": CnyesHeaders}
+    base_url = "https://news.cnyes.com/news/id"
 
     @override
     def parse(
         self, response: HtmlResponse
     ) -> Generator[Union[Item, Request], None, None]:
-        from ..processer.cnyes import CnyesWebSiteNewsProcesser
+        from ..items import CnyesNewsItem
+        from ..utils._datetime import timestamp_to_datetime
+        from ..utils._html import get_text
 
         items: dict[str, Any] = response.json()["items"]  # type: ignore
-        news_generator = CnyesWebSiteNewsProcesser.process(all_news=items["data"])
 
-        try:
-            while True:
-                yield next(news_generator)
+        for news in items["data"]:
+            item = CnyesNewsItem()
 
-        except StopIteration as error:
-            state = error.value
-            next_page_url: Optional[str] = items.get("next_page_url", None)
+            item["news_url"] = f'{self.base_url}/{news["newsId"]}'
+            item["title"] = news["title"]
+            item["content"] = get_text(html_string=news["content"], element="p")
+            item["date"] = timestamp_to_datetime(news["publishAt"])
 
-            if state["processing_complete"]:
-                self.logger.info("成功")
+            yield item
 
-                if next_page_url:
-                    yield Request(
-                        url=urljoin(response.url, next_page_url), callback=self.parse
-                    )
+        # try:
+        #     while True:
+        #         yield next(news_generator)
 
-            else:
-                # raise StopIteration(
-                #     f"已超過上一次的運行時間 '{datetime_to_date_string(last_start_time, '%Y%m%d %H%M')}'"
-                # )
-                self.logger.info(msg="已超過上一次的運行時間")
+        # except StopIteration as error:
+        #     state = error.value
+        #     next_page_url: Optional[str] = items.get("next_page_url", None)
+
+        #     if state["processing_complete"]:
+        #         self.logger.info("成功")
+
+        #         if next_page_url:
+        #             yield Request(
+        #                 url=urljoin(response.url, next_page_url), callback=self.parse
+        #             )
+
+        #     else:
+        #         # raise StopIteration(
+        #         #     f"已超過上一次的運行時間 '{datetime_to_date_string(last_start_time, '%Y%m%d %H%M')}'"
+        #         # )
+        #         self.logger.info(msg="已超過上一次的運行時間")
