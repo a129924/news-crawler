9b8224fd170bab89e0b300ccb0919d5ad8fe59a6
a129924 <a129924@gmail.com>
2024-08-07 22:33:01 +0800

    feat: load_json_file 改成load方法 feat: 新增write_json_file 輸出json

--- news_crawler/utils/_json.py
+++ news_crawler/utils/_json.py
@@ -3,6 +3,13 @@ from typing import Any
 
 def load_json_file(json_filepath: str) -> Any:
     with open(json_filepath, mode="r+", encoding="UTF-8") as json_file:
-        from json import loads
+        from json import load
 
-        return loads(json_file.read())
+        return load(json_file)
+
+
+def write_json_file(json_filepath: str, obj: Any) -> None:
+    with open(json_filepath, mode="w+", encoding="UTF-8") as json_file:
+        from json import dumps
+
+        json_file.write(dumps(obj, ensure_ascii=False))
