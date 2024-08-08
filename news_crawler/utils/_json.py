2cc0f82938086af20b28382b2847ba6b3ea6f12b
a129924 <a129924@gmail.com>
2024-08-08 14:39:01 +0800

    feat: 使用dump寫json file

--- news_crawler/utils/_json.py
+++ news_crawler/utils/_json.py
@@ -10,6 +10,6 @@ def load_json_file(json_filepath: str) -> Any:
 
 def write_json_file(json_filepath: str, obj: Any) -> None:
     with open(json_filepath, mode="w+", encoding="UTF-8") as json_file:
-        from json import dumps
+        from json import dump
 
-        json_file.write(dumps(obj, ensure_ascii=False))
+        dump(obj, json_file, ensure_ascii=False)
