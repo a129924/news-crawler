98378c27cd18ee38d5b328d9e85793d8f9a399fc
a129924 <a129924@gmail.com>
2024-08-09 15:14:30 +0800

    feat: 新增意外錯誤發生時 有一個檢查機制可以查詢

--- news_crawler/processer/states.py
+++ news_crawler/processer/states.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from pathlib import Path
-from typing import Literal, Optional, Union
+from typing import Literal, Union
 
 from typing_extensions import TypeAlias, TypedDict
 
@@ -9,17 +9,17 @@ CrawlWebsite: TypeAlias = Literal["cynes"]
 
 class WebsiteCrawlState(TypedDict):
     name: CrawlWebsite
-    last_start_time: datetime
+    last_start_time: Union[datetime, str]
+    processed_items: set[str]  # set[{news_title}{news_date}]
 
 
 class CrawlStateProcesser:
     def __init__(self, base_filepath: Union[str, Path], name: CrawlWebsite) -> None:
         self.website_name: CrawlWebsite = name
-        self.start_time = datetime.now()
 
         base_path = Path(base_filepath)
         self.state_fullpath = base_path / "states" / f"{name}.crawler.state"
-        print(f"{self.state_fullpath = }")
+
         self.state = self.load_state()
 
     def load_state(self) -> WebsiteCrawlState:
@@ -35,24 +35,58 @@ class CrawlStateProcesser:
         try:
             state = load_json_file(self.state_fullpath.__str__())
             state["last_start_time"] = datetime.fromisoformat(state["last_start_time"])
-            print(f"{state = }")
+            state["processed_items"] = set(state["processed_items"])
 
             return state
-        except FileNotFoundError:
+        except (FileNotFoundError, KeyError):
             return WebsiteCrawlState(
-                name=self.website_name, last_start_time=datetime.now()
+                name=self.website_name,
+                last_start_time=datetime.now(),
+                processed_items=set(),
             )
 
-    def write_state(self, start_time: Optional[datetime] = None) -> None:
+    @staticmethod
+    def generate_item_key(title: str, date: Union[str, datetime]) -> str:
+        return f"{title}{date}"
+
+    def mark_item_processed(self, item_key: str):
+        """
+        mark_item_processed 新增以爬取成功的新聞
+
+        Args:
+            item_key (str): 爬取成功的key({news_title}{news_date})
+        """
+        self.state["processed_items"].add(item_key)
+
+    def is_item_processed(self, item_key: str) -> bool:
+        """
+        is_item_processed item_key是否在processed_items存在
+
+        Args:
+            item_key (str): 爬取成功的key({news_title}{news_date})
+
+        Returns:
+            bool: 是否存在
+        """
+        return item_key in self.state["processed_items"]
+
+    def write_state(self, is_success: bool, success_or_error_time: datetime) -> None:
         """
         write_state 輸出新的結果到state file
+
+        Args:
+            is_success (bool): 是否完成
+            success_or_error_time (datetime): 輸出的datetime
         """
+
         from ..utils._json import write_json_file
+
+        self.state["last_start_time"] = success_or_error_time.isoformat()
+
+        if is_success:
+            self.state["processed_items"] = set()
 
         write_json_file(
             self.state_fullpath.__str__(),
-            obj={
-                "name": self.website_name,
-                "last_start_time": (start_time or self.start_time).isoformat(),
-            },
+            obj=self.state,
         )
