8ccb2c5037fd3b346945a2bfc62a111e2b1523bb
a129924 <a129924@gmail.com>
2024-08-09 15:16:04 +0800

    feat: 為了會有可能意外錯誤產生 新增對應方法

--- news_crawler/pipelines/time_boundary.py
+++ news_crawler/pipelines/time_boundary.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 from typing import Optional
 
-from scrapy.exceptions import CloseSpider
+from scrapy import Item
+from scrapy.exceptions import CloseSpider, DropItem
 from scrapy.spiders import Spider
 from typing_extensions import override
 
@@ -18,26 +19,24 @@ class TimeBoundaryPipeline(BasePipeline):
         self.last_start_time: Optional[datetime] = None
 
         self._is_sucess = True
-        self.last_error_date: Optional[datetime] = None
+        self.start_time = datetime.now()
 
     @override
     def open_spider(self, spider: Spider):
-        print("open_spdier", "spider.name", spider.name)
-
         self.state_processer = CrawlStateProcesser(
             base_filepath=PROJECT_BASE_PATH,
             name=spider.name,  # type: ignore
         )
 
-        self.last_start_time = self.state_processer.state["last_start_time"]
+        self.last_start_time = self.state_processer.state["last_start_time"]  # type: ignore
 
     @override
-    def process_item(self, item, spider):
+    def process_item(self, item: Item, spider) -> Item:
         """
         處理每一個從爬蟲中傳遞過來的 item。如果 item 的時間早於上次啟動時間，則關閉爬蟲。
         :param item: 從爬蟲傳遞過來的 item
         :param spider: 當前處理 item 的爬蟲對象
-        :return: 返回處理後的 item 或者 raise CloseSpider 異常
+        :return: 返回處理後的 item 或者 raise CloseSpider 或者raise DropItem 異常
         """
         from ..utils._datetime import datetime_to_date_string
 
@@ -46,17 +45,31 @@ class TimeBoundaryPipeline(BasePipeline):
         try:
             if publish_at < self.last_start_time:  # type: ignore
                 # 如果 item 的時間早於上次啟動時間，關閉爬蟲
-
                 raise CloseSpider("Reached the last start time threshold")
 
+            item_key = self.state_processer.generate_item_key(
+                title=item["title"], date=item["date"]
+            )
+
+            if self.state_processer.is_item_processed(item_key=item_key):
+                raise DropItem(f"Duplicate item found: {item['title']}")
+
             item["date"] = datetime_to_date_string(publish_at, format="%Y%m%d %H%M")
+            self.state_processer.mark_item_processed(item_key=item_key)
 
             return item
 
-        except Exception:
+        except Exception as error:
+            # 如果有意外異常 直接報錯 並且回寫已經爬取完畢
             self._is_sucess = False
-            self.last_error_date = publish_at
+
+            raise error
 
     @override
     def close_spider(self, spider):
-        self.state_processer.write_state(self.last_error_date)
+        self.state_processer.write_state(
+            is_success=self._is_sucess,
+            success_or_error_time=self.start_time
+            if self._is_sucess
+            else self.last_start_time,  # type: ignore
+        )
