daa1067bd7d0f20797c72b4a9a71d7644a0a3cb6
a129924 <a129924@gmail.com>
2024-08-16 11:41:23 +0800

    fix: 無法使用CloseSpider 來讓spider停止運行 必須使用crawler.engine.close_spider(...) 手動關閉

--- news_crawler/pipelines/time_boundary.py
+++ news_crawler/pipelines/time_boundary.py
@@ -2,7 +2,8 @@ from datetime import datetime
 from typing import Optional
 
 from scrapy import Item
-from scrapy.exceptions import CloseSpider, DropItem
+from scrapy.crawler import Crawler
+from scrapy.exceptions import DropItem
 from scrapy.spiders import Spider
 from typing_extensions import override
 
@@ -14,12 +15,27 @@ __all__ = ["TimeBoundaryPipeline"]
 
 
 class TimeBoundaryPipeline(BasePipeline):
-    # FIXME: 要修改因為意外錯誤連跳出紀錄爬取到的新聞日期
-    def __init__(self):
+    @override
+    def __init__(self, crawler: Crawler):
+        self.crawler = crawler
+
         self.last_start_time: Optional[datetime] = None
 
-        self._is_sucess = True
+        self._is_sucess = False
         self.start_time = datetime.now()
+
+    @classmethod
+    def from_crawler(cls, crawler: Crawler):
+        """
+        工廠方法，用於創建 pipeline 實例並傳遞 crawler 對象。
+
+        Args:
+            crawler (Crawler): Scrapy 的 Crawler 實例。
+
+        Returns:
+            BasePipeline: 初始化後的 BasePipeline 實例。
+        """
+        return cls(crawler)
 
     @override
     def open_spider(self, spider: Spider):
@@ -36,34 +52,32 @@ class TimeBoundaryPipeline(BasePipeline):
         處理每一個從爬蟲中傳遞過來的 item。如果 item 的時間早於上次啟動時間，則關閉爬蟲。
         :param item: 從爬蟲傳遞過來的 item
         :param spider: 當前處理 item 的爬蟲對象
-        :return: 返回處理後的 item 或者 raise CloseSpider 或者raise DropItem 異常
+        :return: 返回處理後的 item 或者 raise DropItem 異常
         """
         from ..utils._datetime import datetime_to_date_string
 
         publish_at: datetime = item["date"]
 
-        try:
-            if publish_at < self.last_start_time:  # type: ignore
-                # 如果 item 的時間早於上次啟動時間，關閉爬蟲
-                raise CloseSpider("Reached the last start time threshold")
-
-            item_key = self.state_processer.generate_item_key(
-                title=item["title"], date=item["date"]
+        if publish_at < self.last_start_time:  # type: ignore
+            # 如果 item 的時間早於上次啟動時間，關閉爬蟲
+            self._is_sucess = True
+            self.crawler.engine.close_spider(  # type: ignore
+                spider=spider, reason="Reached the last start time threshold"
             )
 
-            if self.state_processer.is_item_processed(item_key=item_key):
-                raise DropItem(f"Duplicate item found: {item['title']}")
+            raise DropItem("Reached the last start time threshold")
 
-            item["date"] = datetime_to_date_string(publish_at, format="%Y%m%d %H%M")
-            self.state_processer.mark_item_processed(item_key=item_key)
+        item_key = self.state_processer.generate_item_key(
+            title=item["title"], date=item["date"]
+        )
 
-            return item
+        if self.state_processer.is_item_processed(item_key=item_key):
+            raise DropItem(f"Duplicate item found: {item['title']}")
 
-        except Exception as error:
-            # 如果有意外異常 直接報錯 並且回寫已經爬取完畢
-            self._is_sucess = False
+        item["date"] = datetime_to_date_string(publish_at, format="%Y%m%d %H%M")
+        self.state_processer.mark_item_processed(item_key=item_key)
 
-            raise error
+        return item
 
     @override
     def close_spider(self, spider):
