3c13fb78dc747b9e05efe018c715572a2f25a4c2
a129924 <a129924@gmail.com>
2024-08-21 14:27:12 +0800

    feat: 新增_parse_news 處理新聞頁面爬取 feat: 完善_parse_news_list 使用meta傳遞參數 yield self._schedule_next_page() 繼續parse下一頁

--- news_crawler/spiders/udn_spider.py
+++ news_crawler/spiders/udn_spider.py
@@ -1,5 +1,5 @@
 from collections.abc import Generator
-from typing import Iterable, Literal
+from typing import Literal
 
 from scrapy import Request
 from scrapy.http import HtmlResponse
@@ -14,6 +14,7 @@ def api_base_url(page: int) -> str:
 
 
 class UdnSpider(Spider):
+    date_format = "%Y-%m-%d %H:%M"  # "2024-08-21 10:29"
     page = 0
 
     name = "udn"
@@ -22,9 +23,6 @@ class UdnSpider(Spider):
 
     base_url = "https://udn.com"
     data_query_map: dict[Literal["data_query"], str] = {}
-
-    @override
-    def start_requests(self) -> Iterable[Request]: ...
 
     def _get_data_query(self, response: HtmlResponse):
         def mix_key_value(json_string: str) -> str:
@@ -51,11 +49,21 @@ class UdnSpider(Spider):
 
         return f"{api_base_url(page=self.page)}&{self.data_query_map['data_query']}"
 
+    def _schedule_next_page(self) -> Request:
+        next_page_url = self._make_next_api_url()
+
+        return Request(url=next_page_url, callback=self._parse_news_list)
+
+    def _process_news_content(self, response: HtmlResponse) -> str:
+        return "".join(
+            response.css(".article-content__editor p::text").getall()
+        ).replace("\r\n", "")
+
     @override
     def parse(self, response: HtmlResponse) -> Generator[Request, None, None]:
         self._set_data_query(response=response)
 
-        yield Request(url=self._make_next_api_url(), callback=self._parse_news_list)
+        yield self._schedule_next_page()
 
     def _parse_news_list(
         self, response: HtmlResponse
@@ -65,11 +73,24 @@ class UdnSpider(Spider):
             "/news/story/7266/8173730?from=udn-ch1_breaknews-1-0-news"
             """
             yield Request(
-                f"{self.base_url}{news_item['titleLink']}", callback=self._parse_news
+                f"{self.base_url}{news_item['titleLink']}",
+                callback=self._parse_news,
+                meta={
+                    "title": news_item["title"],
+                    "date": news_item["time"]["date"],  # "2024-08-21 10:29"
+                },
             )
 
+        yield self._schedule_next_page()
+
     def _parse_news(self, response: HtmlResponse) -> Generator[NewsItem, None, None]:
+        from datetime import datetime
+
         item = NewsItem()
-        response.meta.get()
+
+        item["news_url"] = response.url
+        item["title"] = response.meta["title"]
+        item["content"] = self._process_news_content(response=response)
+        item["date"] = datetime.strptime(response.meta["date"], self.date_format)
 
         yield item
