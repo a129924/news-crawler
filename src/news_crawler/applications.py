cb678adbc2c0928a1944f50e3172a870f2dae6b8
a129924 <a129924@gmail.com>
2024-08-28 15:45:49 +0800

    doc: 新增文件敘述

--- src/news_crawler/applications.py
+++ src/news_crawler/applications.py
@@ -1,4 +1,8 @@
 from collections.abc import Generator, Iterable
+from contextlib import AbstractContextManager
+
+from psycopg import Cursor
+from typing_extensions import Self
 
 from .parse_company.parse import Company
 
@@ -7,7 +11,13 @@ class Application:
     def __init__(self, db_config: dict[str, str]):
         self.db_config = db_config
 
-    def connect_db(self):
+    def connect_db(self) -> Self:
+        """
+        connect_db
+
+        Returns:
+            Self: self
+        """
         from .db.connect_db import create_connection
 
         self.connection = create_connection(
@@ -18,26 +28,61 @@ class Application:
 
         return self
 
-    def get_cursor(self):
+    def get_cursor(self) -> AbstractContextManager[Cursor]:
+        """
+        get_cursor
+
+        Returns:
+            ContextManager[Cursor]: cursor
+        """
         from .db.connect_db import get_cursor
 
         return get_cursor(self.connection)
 
-    def parse_company_data(self, filter_company_name: Iterable[str] = {"上市", "上櫃"}):
+    def parse_company_data(
+        self, filter_company_name: Iterable[str] = {"上市", "上櫃"}
+    ) -> Generator[Company, None, None]:
+        """
+        parse_company_data
+
+        Args:
+            filter_company_name (Iterable[str], optional): filter_company_name. Defaults to {"上市", "上櫃"}.
+
+        Returns:
+            Generator[Company, None, None]: company data
+        """
         from .parse_company.parse import main
 
         yield from main(filter_company_name=filter_company_name)
 
-    def process_company_data(self, companys: Generator[Company, None, None]):
+    def process_company_data(
+        self, companys: Generator[Company, None, None]
+    ) -> tuple[list[str], list[tuple[str, ...]]]:
+        """
+        process_company_data
+
+        Args:
+            companys (Generator[Company, None, None]): company data
+
+        Returns:
+            tuple[list[str], list[tuple[str, ...]]]: headers, data
+        """
         first_company = next(companys)
+
         headers = list(first_company.keys())
         data = [tuple(first_company.values())] + [
             tuple(company.values()) for company in companys
         ]
 
-        return headers, data
+        return headers, data  # type: ignore
 
     def insert_company_data(self, table_name: str):
+        """
+        insert_company_data
+
+        Args:
+            table_name (str): table_name
+        """
         with self.connect_db().get_cursor() as cursor:
             from .db.execute import insert_many_data
 
@@ -50,4 +95,7 @@ class Application:
             )
 
     def pipeline(self):
+        """
+        pipeline
+        """
         self.insert_company_data(table_name="company")
