c147f66ec80691aa94688393f7524b866904a730
a129924 <a129924@gmail.com>
2024-08-28 15:50:04 +0800

    feat: 新增測試table

--- sql/create_company.sql
+++ sql/create_company.sql
@@ -1,4 +1,13 @@
-CREATE TABLE Company (
+CREATE TABLE company (
+    id SERIAL PRIMARY KEY,  -- 添加一个自动递增的主键
+    name VARCHAR(255) NOT NULL,
+    english_name VARCHAR(255) NOT NULL,
+    short_name VARCHAR(100) NOT NULL,
+    market VARCHAR(10) NOT NULL CHECK (market IN ('公開發行', '上市', '上櫃')),
+    code VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE test_company (
     id SERIAL PRIMARY KEY,  -- 添加一个自动递增的主键
     name VARCHAR(255) NOT NULL,
     english_name VARCHAR(255) NOT NULL,
