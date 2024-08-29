fb77e586a0aed1a7fce9684b92be68618ce392aa
a129924 <a129924@gmail.com>
2024-08-29 11:44:18 +0800

    feat: append new  news table

--- sql/create_company.sql
+++ sql/create_company.sql
@@ -1,5 +1,5 @@
 CREATE TABLE company (
-    id SERIAL PRIMARY KEY,  -- 添加一个自动递增的主键
+    id SERIAL PRIMARY KEY,  -- 添加一個自動遞增的主鍵
     name VARCHAR(255) NOT NULL,
     english_name VARCHAR(255) NOT NULL,
     short_name VARCHAR(100) NOT NULL,
@@ -8,10 +8,18 @@ CREATE TABLE company (
 );
 
 CREATE TABLE test_company (
-    id SERIAL PRIMARY KEY,  -- 添加一个自动递增的主键
+    id SERIAL PRIMARY KEY,  -- 添加一個自動遞增的主鍵
     name VARCHAR(255) NOT NULL,
     english_name VARCHAR(255) NOT NULL,
     short_name VARCHAR(100) NOT NULL,
     market VARCHAR(10) NOT NULL CHECK (market IN ('公開發行', '上市', '上櫃')),
     code VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE news (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    news_url VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL
 );
